dff742b481cba1422dc5d5ea80dbfd87dc3ffdb6
Ashwin Mridul <ashwin.mridul@hotelsoft.com>
2018-08-22 11:18:58 +0530

    Added redirect on request timeout condition

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const {Request, Annotation} = require("zipkin");
-const initalizeZipkin =  require("./src/initalizeZipkin");
+const {
+  Request,
+  Annotation
+} = require("zipkin");
+const initalizeZipkin = require("./src/initalizeZipkin");
 const _ = require("lodash");
 export default (zipkinUrl, _remoteServiceName, _serviceName) =>
   req => {
-if (!zipkinUrl || zipkinUrl.length > 0 && zipkinUrl.trim() === "") {
+    if (!zipkinUrl || zipkinUrl.length > 0 && zipkinUrl.trim() === "") {
       throw new Error("zipkin destination url should be valid");
     }
     let zipkinInstance = initalizeZipkin.default;
-    var {tracer, serviceName = (_serviceName) ? _serviceName: "unknwown", remoteServiceName} = zipkinInstance(zipkinUrl, _remoteServiceName);
-    var traceId = null;
-    tracer.scoped(function () {
+    let {
+      tracer,
+      serviceName = (_serviceName) ? _serviceName : "unknwown",
+      remoteServiceName
+    } = zipkinInstance(zipkinUrl, _remoteServiceName);
+    let traceId = null;
+    tracer.scoped(function() {
       tracer.setId(tracer.createChildId());
       traceId = tracer.id;
       const wrappedOptions = Request.addZipkinHeaders(req, traceId);
@@ -17,16 +24,16 @@ if (!zipkinUrl || zipkinUrl.length > 0 && zipkinUrl.trim() === "") {
       if (!_serviceName) {
         let modifiedServiceName = req.url;
         if (req.url) {
-            let tempUrl = req.url;
-            let splitUrls  = _.split(tempUrl, "/");
-            if(splitUrls.length > 0) {
-              let lastVal = splitUrls[splitUrls.length - 1];
-              let host = splitUrls.length >= 3 ? splitUrls[2]: tempUrl;
-              splitUrls[splitUrls.length - 1] = _.split(lastVal, "?")[0];
-              let callName = splitUrls.length >= 3 ? _.join(_.drop(splitUrls, 3), "/"): tempUrl;
-              modifiedServiceName = `host: ${host} - call: ${callName}`;
-            }
-            serviceName = modifiedServiceName;
+          let tempUrl = req.url;
+          let splitUrls = _.split(tempUrl, "/");
+          if (splitUrls.length > 0) {
+            let lastVal = splitUrls[splitUrls.length - 1];
+            let host = splitUrls.length >= 3 ? splitUrls[2] : tempUrl;
+            splitUrls[splitUrls.length - 1] = _.split(lastVal, "?")[0];
+            let callName = splitUrls.length >= 3 ? _.join(_.drop(splitUrls, 3), "/") : tempUrl;
+            modifiedServiceName = `host: ${host} - call: ${callName}`;
+          }
+          serviceName = modifiedServiceName;
         }
       }
       tracer.recordServiceName(serviceName);
@@ -49,6 +56,25 @@ if (!zipkinUrl || zipkinUrl.length > 0 && zipkinUrl.trim() === "") {
       });
     });
     req.on("error", (err) => {
+      const timeoutStatus = 314;
+      if (err.status === timeoutStatus) {
+        try {
+          location.href = JSON.parse(err.response.text).location;
+        } catch (newLocationErr) {
+          console.error("error in finding new location");
+          //location.href = window.location.origin;
+        }
+      } else if (err.originalError && err.originalError.status === timeoutStatus) {
+        try {
+          location.href = JSON.parse(err.originalError.response.text).location;
+        } catch (newLocationErr) {
+          console.error("error in finding new location");
+          //location.href = window.location.origin;
+        }
+      } else {
+        console.info("Error in middleware API", err);
+        throw err;
+      }
       tracer.scoped(() => {
         tracer.setId(traceId);
         tracer.recordBinary("request.error", err);
